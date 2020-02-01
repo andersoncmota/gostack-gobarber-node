e688b00bee7bedbd35bfbf62398535ce08b9566a
Anderson Mota <andersonmota.acm@gmail.com>
2020-01-31 22:31:58 -0300

    Configuring Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3353a4efaa3a2b',
+    pass: 'fd73aaa128c418',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
